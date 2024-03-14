8ce1b770236164d9c9504d7f4e5383358c0c0391
Julian Victor <julianvictorlopesoliveira@gmail.com>
2024-03-14 16:23:21 -0300

    Foi iniciada a validação de dados

---
+++ js/script.js
@@ -0,0 +1,57 @@
+function validar() {
+    console.log("teste");
+
+
+    const nome = document.getElementById("nome");
+    const sobrenome = document.getElementById("sobrenome");
+    const nasc = document.getElementById("nasc");
+    const curso = document.getElementById("curso");
+    const matutino = document.getElementById("matutino");
+    const vespertino = document.getElementById("vespertino");
+    const noturno = document.getElementById("noturno");
+
+
+    if (nome.value == "") {
+        document.getElementById("nome").focus();
+        alert("Não deixe o campo nome vazio.");
+        return false;
+    }
+
+    if (sobrenome.value == "") {
+        document.getElementById("sobrenome").focus();
+        alert("Não deixe o campo sobrenome vazio.");
+        return false;
+    }
+
+    if (nasc.value == "") {
+        document.getElementById("nasc").focus();
+        alert("Não deixe o campo ano de nascimento vazio.");
+        return false;
+    }
+
+    if (curso.value == "") {
+        document.getElementById("curso").focus();
+        alert("Não deixe o campo curso vazio.");
+        return false;
+    }
+
+    if (matutino.value == "") {
+        document.getElementById("matutino").focus();
+        alert("Não deixe o campo periodo vazio.");
+        return false;
+    }
+
+    if (vespertino.value == "") {
+        document.getElementById("vespertino").focus();
+        alert("Não deixe o campo periodo vazio.");
+        return false;
+    }
+
+    if (noturno.value == "") {
+        document.getElementById("noturno").focus();
+        alert("Não deixe o campo periodo vazio.");
+        return false;
+    }
+
+    alert("Sucesso");    
+}
